887d52269e3785e2b5ee9f391412770c57b18740
Matan Gover <matangover@gmail.com>
2018-04-30 11:17:23 -0400

    Add Score model and admin site.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    name = models.CharField(max_length=255)
+    scan = models.FileField()
+    transcription = models.FileField()
